6a7f2ba2fe85ad15f52d02eabf6d47d4fba4ab8d
Gustavo Santos <gefgu@hotmail.com>
2024-08-11 08:08:24 -0300

    feat: implment Elmento.hpp functions

--- include/Listas/Elemento.hpp
+++ include/Listas/Elemento.hpp
@@ -16,4 +16,49 @@ public:
   void setProximo(Elemento<TE> *prox);
 }
 
+template <class TE>
+Elemento<TE>::Elemento(TE *elem) : pInfo(NULL), pProx(NULL),
+{
+  if (elem)
+  {
+    pInfo = elem;
+  }
+  else
+  {
+    cout << "Ponteiro inválido na criação do Elemento" << endl;
+  }
+}
+
+template <class TE>
+Elemento<TE>::~Elemento()
+{
+  if (pInfo)
+    delete pInfo;
+}
+
+template <class TE>
+TE *Elemento<TE>::getInfo()
+{
+  return pInfo;
+}
+
+template <class TE>
+Elemento<TE> *Elemento<TE>::getProximo()
+{
+  return pProx;
+}
+
+template <class TE>
+Elemento<TE> *Elemento<TE>::setProximo(Elemento<TE> *prox)
+{
+  if (prox)
+  {
+    pProx = prox;
+  }
+  else
+  {
+    cout << "Ponteiro inválido em Elemento.setProximo()" << endl;
+  }
+}
+
 #endif // ELEMENTO_HPP
